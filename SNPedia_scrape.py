86fac1aa0983788e0caf8f8c52886f7c1d5c846d
doerlbh <doerlbh@gmail.com>
2016-07-23 20:10:24 -0700

    update code

--- SNPedia_scrape.py
+++ SNPedia_scrape.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #! python3
 # SNPedia_scrape
 # To download rs information from SNPedia automatically
@@ -6,7 +7,7 @@
 
 # read in the list and parse the parameters
 
-import webbrowser, sys, pyperclip, requests
+import webbrowser, sys, pyperclip, requests, subprocess
 if len(sys.argv) > 1:
     # Get address from command line.
     rs = ' '.join(sys.argv[1:])
@@ -14,58 +15,19 @@ else:
     # Get address from clipboard.
     rs = pyperclip.paste()
 
-webbrowser.open('http://www.snpedia.com/index.php/' + rs)
+#webbrowser.open('http://www.snpedia.com/index.php/' + rs)
 
 resmsg = requests.get('http://www.snpedia.com/index.php/' + rs)
 resmsg.raise_for_status()
-outFile = open(rs + '-out.txt', 'wb')
-for chunk in res.iter_content(1000000):
+
+outfname1 = rs + '-out.txt'
+outfname2 = rs + '-PMID.txt'
+
+
+outFile = open(outfname1, 'wb')
+for chunk in resmsg.iter_content(1000000):
         outFile.write(chunk)
 outFile.close()
 
-
-lrawname = argv[1]
-lname = lrawname[:-5]
-lstr = ".list"
-ostr = "_matrixB.out"
-
-f=open(path + lname + lstr,'r')
-flist=f.readlines()
-flist=list(set(flist))
-f.close()
-
-# sample finely around the parameters
-
-clist = []
-count = 0
-
-for bb in flist:
-    A = bb.split('_')[0]
-    B = bb.split('_')[1]
-    C = bb.split('_')[2]
-    Bran = [x*0.2 for x in range(5*(int(B)-2), 5*(int(B)+2)+1)]
-    for Bi in Bran:
-    	clist.insert(count, A+' '+str(Bi)+' '+C+'\n')
-    	count+=1
-    
-
-# set range
-
-
-# make combinations
-
-  
-# remove duplicates
-
-clist = list(set(clist))
-
-#print clist
-
-# write into file
-                         
-f=open(path + lname + ostr,'w')
-f.writelines(clist)
-f.close()
-
-raw_input("Press<enter>")
-
+bash = "grep ‘>PMID’ " + outfname1 + " > " + outfname2 + "; rm " + outfname1
+subprocess.Popen(bash)
