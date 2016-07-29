ac35015eae8d562711a8e36bda1fc1d5c00883bf
doerlbh <doerlbh@gmail.com>
2016-07-29 01:20:34 -0700

    update code

--- SNPedia_info.sh
+++ SNPedia_info.sh
@@ -22,7 +22,7 @@ for k in new_vir_list_*; do
 		sed -i '' "s#<\/a##g" $j-Gene;
 
 		for l in "cat $j-Gene"; do
-			python SNPedia_scrape.py $l || echo no further info on $l
+			python SNPedia_scrape.py $l || echo no further info on $l > $j-Info
 
 			grep "<strong class=\"selflink\">" $l-out.txt >> $j-Info
 			sed -i '' "s#<strong class=\"selflink\">##g" $j-Info;
@@ -34,17 +34,18 @@ for k in new_vir_list_*; do
 
 			#echo "Mutations in the SCN1A< gene have been associated with <a href=\"/index.php/Severe_myoclonic_epilepsy_in_infancy\" title=\"Severe myoclonic epilepsy in infancy\">Severe myoclonic epilepsy in infancy</a> (SMEI) and <a href=\"/index.php?title=Dravet_syndrome&amp;action=edit&amp;redlink=1\" class=\"new\" title=\"Dravet syndrome (page does not exist)\">Dravet syndrome</a>, forms of <a href=\"/index.php/Epilepsy\" title=\"Epilepsy\">epilepsy</a>. <a rel=\"nofollow\" class=\"external text\" href=\"http://www.washingtonpost.com/national/health-science/medical-mysteries-seizures-hit-baby-girl-soon-after-she-had-routine-shots/2011/12/21/gIQAfkbAdQ_story_2.html\">Washington Post article</a> <a rel=\"nofollow\" class=\"external autonumber\" href=\"http://dravet.org/\">[1]</a> <a rel=\"nofollow\" class=\"external text\" href=\"http://www.ncbi.nlm.nih.gov/books/NBK1318/\">NCBI Bookshelf</a>" | tr -s '<\a href' '\n'
 
-			rm $l-out.txt
+			rm $l-out.txt;
 		done
 
-		cat $j-Gene | tr '\n' "," > $j-Gene-all
+		cat $j-Gene | tr '\n' "," > $j-Gene-all;
+		sed -i -e "s/$(printf '\t')//" $j-Gene-all;
 
     	#<tr><td width="90">Chromosome</td><td>2</td></tr>
-		sed -i '' "s#<tr><td width="90">Chromosome</td><td>#$(printf '\t')#g" $j-Chr;
+		sed -i '' "s#<tr><td width=\"90\">Chromosome</td><td>#$(printf '\t')#g" $j-Chr;
 		sed -i '' "s#</td></tr>#$(printf '\t')#g" $j-Chr;
 
 		#<tr><td width="90">Position</td><td>166036278</td></tr>
-		sed -i '' "s#<tr><td width="90">Position</td><td>#$(printf '\t')#g" $j-Pos;
+		sed -i '' "s#<tr><td width=\"90\">Position</td><td>#$(printf '\t')#g" $j-Pos;
 		sed -i '' "s#</td></tr>#$(printf '\t')#g" $j-Pos;
 
 		grep ">PMID"  $j-out.txt > $j-PMID;
@@ -54,10 +55,13 @@ for k in new_vir_list_*; do
 		sed -i '' "s#</a><a href=\"/index.php/File:OA-icon.png\" class=\"image\"><img alt=\"OA-icon.png\" src=\"https://media.snpedia.com/images/5/5b/OA-icon.png\" width=\"15\" height=\"15\" />#$(printf '\t')#g" $j-PMID;
 		sed -i '' "s#</a>]#$(printf '\t')#g" $j-PMID;
 
-		sed -i -e "s/^/$j$(printf '\t')$(printf ${k:13})$(printf '\t')$(cat $j-Chr)$(printf '\t')$(cat $j-Pos)$(printf '\t')$(cat $j-Gene-all)$(printf '\t')$(cat $j-Info)/" $j-PMID;
+		#sed -i -e "s/^/$j$(printf '\t')$(printf ${k:13})$(printf '\t')$(cat $j-Chr)$(printf '\t')$(cat $j-Pos)$(printf '\t')$(cat $j-Gene-all)$(printf '\t')$(cat $j-Info)/" $j-PMID;
+		sed -i -e "s/^/$j$(printf '\t')${k:13}$(printf '\t')$(cat $j-Chr)$(printf '\t')$(cat $j-Pos)$(printf '\t')$(cat $j-Gene-all)$(printf '\t')$(cat $j-Info)/" $j-PMID;
 		
 		sed -i -e "s/$(printf '\t')$(printf '\t')/$(printf '\t')/" $j-PMID;
 		
+		sed -i -e "s/$(printf '\t')$(printf '\t')/$(printf '\t')/" $j-PMID;
+
 		rm $j-out.txt;
 		
 	#echo $j is good.
@@ -66,13 +70,14 @@ for k in new_vir_list_*; do
 
 	cat rs* >> tempPMIDlist;
 	cat Rs* >> tempPMIDlist;
-	sort tempPMIDlist$now > tempPMID$now.sort;
-	uniq -d tempPMID$now.sort > tempPMID$now.uniq;
-	grep "Rs"  tempPMID$now.uniq > ${k//vir/PMID}_$now.uniq;
+	sort tempPMIDlist > tempPMID.sort;
+	uniq -d tempPMID.sort > tempPMID.uniq;
+	grep "Rs"  tempPMID.uniq > ${k//vir/PMID}.uniq;
+	grep "rs"  tempPMID.uniq >> ${k//vir/PMID}.uniq;
 	rm temp*;
 	rm rs*;
 	rm Rs*;
-	rm *out.txt
+	#rm *out.txt
 
 	echo $k is finished.~~~~~~~~
 
