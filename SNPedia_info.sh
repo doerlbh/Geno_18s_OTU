dd73e040126b8620e4fd8383dc009603174d30e0
doerlbh <doerlbh@gmail.com>
2016-07-28 22:11:43 -0700

    update code

--- SNPedia_info.sh
+++ SNPedia_info.sh
@@ -14,6 +14,13 @@ for k in `cat vir_list*`; do
 		python SNPedia_scrape.py $j;
 		#echo $j-out.txt;
 		
+		grep "Chromosome</td>" $j-out.txt > $j-info;
+		grep "Position</td>" $j-out.txt >> $j-info;
+		grep "Gene</td>" $j-out.txt >> $j-info;
+		<tr><td width="90">
+
+s='<tr><td width="90">Gene</td><td><a href="/index.php/Special:FormEdit/Gene/LOC102724058?redlink=1" class="new" title="LOC102724058 (page does not exist)">LOC102724058</a>, <a href="/index.php/SCN1A" title="SCN1A">SCN1A</a></td></tr>'
+grep -oP '(?<=>).*?(?=</a>,)' <<< $s
 		grep ">PMID"  $j-out.txt > $j-PMID;
 		sed -i '' 's/.*PMID/PMID/g' $j-PMID;
 		sed -i '' "s#</a>] #$(printf '\t')#g" $j-PMID;
