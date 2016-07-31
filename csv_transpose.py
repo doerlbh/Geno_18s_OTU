fb2d881b56547f65460c23cc896a80e0c682bd05
doerlbh <doerlbh@gmail.com>
2016-07-31 03:41:48 -0700

    update code

--- csv_transpose.py
+++ csv_transpose.py
@@ -20,24 +20,3 @@ from itertools import izip
 
 a = izip(*csv.reader(open(input, "rb")))
 csv.writer(open("T-" + input, "wb")).writerows(a)
-
-#webbrowser.open('http://www.snpedia.com/index.php/' + rs)
-
-resmsg = requests.get('http://www.snpedia.com/index.php/' + rs)
-resmsg.raise_for_status()
-
-outfname1 = rs + '-out.txt'
-#outfname2 = rs + '-PMID.txt'
-
-
-outFile = open(outfname1, 'wb')
-for chunk in resmsg.iter_content(10000000):
-        outFile.write(chunk)
-outFile.close()
-
-#bash = "grep \">PMID\" " + outfname1 + " > " + outfname2 + "; rm " + outfname1
-#subprocess.Popen(bash)
-
-
-import openpyxl
-
--- dbpSHP_csv.sh
+++ dbpSHP_csv.sh
@@ -4,13 +4,13 @@
 
 for i in *csv; do
 	cp $i new-$i
-	sed -i '' "s/)/)\",\"/g" new-$i;
-	sed -i '' "s/\"ss.*-HAPMAP\",//g" new-$i;
-	sed -i '' "s/,\"      ShowHide          \"//g" new-$i;
-	sed -i '' "s/\",\"\",\"/\",\"/g" new-$i;
-	sed -i '' "s/\"ssID\",\"Submitter\",//g" new-$i;
-	sed -i '' "s/(count)\",\"/(count)\",\"\",\"/g" new-$i;
-	sed -i '' "s/\"Genotype detail\"/\"\",\"Genotype detail\"/g" new-$i;
+	sed -e '' "s/)/)\",\"/g" new-$i;
+	sed -e '' "s/\"ss.*-HAPMAP\",//g" new-$i;
+	sed -e '' "s/,\".*ShowHide.*\"//g" new-$i;
+	sed -e '' "s/\",\"\",\"/\",\"/g" new-$i;
+	sed -e '' "s/\"ssID\",\"Submitter\",//g" new-$i;
+	sed -e '' "s/(count)\",\"/(count)\",\"\",\"/g" new-$i;
+	sed -e '' "s/\"Genotype detail\"/\"\",\"Genotype detail\"/g" new-$i;
 	python /Users/DoerLBH/Dropbox/git/Geno_SNP_18s_OTU/csv_transpose.py new-$i
 done
 
