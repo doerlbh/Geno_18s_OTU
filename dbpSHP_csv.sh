2290015c7eefecba4b536a7ccd29325c9f4f7aa3
doerlbh <doerlbh@gmail.com>
2016-07-31 03:49:59 -0700

    update code

--- dbpSHP_csv.sh
+++ dbpSHP_csv.sh
@@ -6,14 +6,13 @@ for i in *csv; do
 	cp $i new-$i
 	sed -i '' "s/)/)\",\"/g" new-$i;
 	sed -i '' "s/\"ss.*-HAPMAP\",//g" new-$i;
+	sed -i '' "s/\"ss.*-\",//g" new-$i;
 	sed -i '' "s/,\"      ShowHide          \"//g" new-$i;
 	sed -i '' "s/\",\"\",\"/\",\"/g" new-$i;
 	sed -i '' "s/\"ssID\",\"Submitter\",//g" new-$i;
+	sed -i '' "s/,\"Genotype detail\"//g" new-$i;
 	sed -i '' "s/(count)\",\"/(count)\",\"\",\"/g" new-$i;
-	sed -i '' "s/\"Genotype detail\"/\"\",\"Genotype detail\"/g" new-$i;
 	python /Users/DoerLBH/Dropbox/git/Geno_SNP_18s_OTU/csv_transpose.py new-$i
 done
-
-
 
 
