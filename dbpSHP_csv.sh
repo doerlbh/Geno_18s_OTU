969f2965885e6e673b4610879336e7ba032acede
doerlbh <doerlbh@gmail.com>
2016-07-31 03:26:15 -0700

    update code

--- dbpSHP_csv.sh
+++ dbpSHP_csv.sh
@@ -2,8 +2,6 @@
 # Baihan Lin
 # July 2016
 
-now=$(date +"%Y%m%d");
-
 for i in *csv; do
 	cp $i new-$i
 	sed -i '' "s/)/)\",\"/g" new-$i;
@@ -13,20 +11,9 @@ for i in *csv; do
 	sed -i '' "s/\"ssID\",\"Submitter\",//g" new-$i;
 	sed -i '' "s/(count)\",\"/(count)\",\"\",\"/g" new-$i;
 	sed -i '' "s/\"Genotype detail\"/\"\",\"Genotype detail\"/g" new-$i;
+	python csv_transpose.py new-$i
 done
 
-#for k in *e; do
-#	cat $k ${k//-e/} > ${k//-e/.combo};
-#	rm $k;
-#done
 
-cat Rs* >> PMIDlist$now;
-sort PMIDlist$now > PMID$now.sort;
-uniq -d PMID$now.sort > PMID$now.uniq;
-
-grep "Rs"  PMID$now.uniq > PMIDRs$now.uniq
-
-#cat *PMID.combo >> PMIDlist$now;
-rm Rs*;
 
 
